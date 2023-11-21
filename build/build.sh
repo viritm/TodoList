c801144c39024654cae63d09dd0796923e8e82f0
zheka <buivolov.evgeny@mail.ru>
2023-11-21 13:16:45 +0300

    added script for building on linux and modified build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cmake -DCMAKE_INSTALL_PREFIX="path/to/save" -DCMAKE_TOOLCHAIN_FILE="<path/to/vcpkg>/scripts/buildsystems/vcpkg.cmake" ..
 cmake --build . 
 cmake --install . --config Debug
